1df38969ddf15fab963cf98ec2f105d75014dfd9
traaitt <traaitt@icloud.com>
2020-05-31 15:25:25 +0200

    https://traaitt.com

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -32,7 +32,7 @@ namespace WalletConfig
 
     /* The name of service/walletd, the programmatic rpc interface to a
        wallet */
-    const std::string walletdName = "traaittxte$service";
+    const std::string walletdName = "xteservice";
 
     /* The full name of your crypto */
     const std::string coinName = std::string(CryptoNote::CRYPTONOTE_NAME);
